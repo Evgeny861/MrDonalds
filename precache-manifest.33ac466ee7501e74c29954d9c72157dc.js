792ab5fe28002d49752ca41ba5a0103354422642
Evgeny Tunnikov <tunnikov2013@yandex.ru>
2020-08-25 16:56:09 +0300

    Updates

---
+++ precache-manifest.33ac466ee7501e74c29954d9c72157dc.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "749d43e2ae2766aecd354fb66b0fe06d",
+    "url": "/MrDonalds/index.html"
+  },
+  {
+    "revision": "a7802e46bfb060a94494",
+    "url": "/MrDonalds/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e8ed89400a6fd1166e5a",
+    "url": "/MrDonalds/static/js/2.3195e848.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/MrDonalds/static/js/2.3195e848.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7802e46bfb060a94494",
+    "url": "/MrDonalds/static/js/main.6052da34.chunk.js"
+  },
+  {
+    "revision": "1e00f0e6c8d4d7aac748",
+    "url": "/MrDonalds/static/js/runtime-main.99eb0625.js"
+  },
+  {
+    "revision": "73fbbbd3f07e6654f1ffaf02f2fa582c",
+    "url": "/MrDonalds/static/media/banner.73fbbbd3.png"
+  },
+  {
+    "revision": "cfe37dbc7d784a341d33f03dba29f608",
+    "url": "/MrDonalds/static/media/logo.cfe37dbc.svg"
+  },
+  {
+    "revision": "6ea25215d505d786c94a8f392b4eef5f",
+    "url": "/MrDonalds/static/media/sign.6ea25215.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/MrDonalds/precache-manifest.33ac466ee7501e74c29954d9c72157dc.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/MrDonalds/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
